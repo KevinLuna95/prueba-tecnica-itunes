7e5aed5ceff63b3f04d0d5884d20ae0f96cb89e0
kevin <kkevinlunaa95@gmail.com>
2022-10-18 12:12:55 +0200

    fin del test

---
+++ README.md
@@ -0,0 +1,23 @@
+Usando la API de Itunes:
+
+Using itunes API
+1-The 10 last released songs by Galantis
+2-The 10 last released songs by one artist you know.
+3-Search for 30 media types with the word “Peace” in the title and extract their artist
+name and media type.
+4-All the songs released by Adele in the year 2021.
+5-The 10 podcasts and movies with the best rating index.
+
+¿Cómo ejecutar el código?
+Para ejecutar el código puedes guardar la carpeta en tu 
+PC y abrir el archivo index.html desde tu navegador.
+También copiando y pegando el código en la siguiente web https://www.cubicfactory.com/jseditor/
+
+Funcionamiento:
+La API es muy restringida, es por eso que solicito más información 
+que la requerida a la API y después segmento mediante restricciones de parámetros
+para dar el resultado esperado.
+
+Si existiese más documentación sobre la API y sus usos podría minimizarse el código
+aportando los parámetros de la API necesarios, pero al ser escasa, hacerlo mediante códigoes la mejor manera de
+solucionarlo que he encontrado.
--- itunes.js
+++ itunes.js
@@ -1,90 +1,101 @@
-"use strict";
+$(document).ready(function(){
 
-var artistwait = "";
-const newestSongs = async() =>{
-    try{
-        let artist = await artistwait;
-        var apiRest = `https://itunes.apple.com/search?term=${artist}&media=music&limit=50`;
-        const response = await fetch(apiRest);
-        const result = await response.json();
-        let dataObject = result.results;
-        dataObject.sort( (a,b) =>{
-            if(a.releaseDate == b.releaseDate){
+    "use strict";
+    
+    var artistwait = "";
+    var id = "#newestSong"
+    const newestSongs = async () => {
+        try {
+            let artist = await artistwait;
+            var apiRest = `https://itunes.apple.com/search?term=${artist}&media=music&limit=50`;
+            const response = await fetch(apiRest);
+            const result = await response.json();
+            let dataObject = result.results;
+        
+        dataObject.sort( (a,b) => {
+            if (a.releaseDate == b.releaseDate) {
                 return 0;
             }
-            if(a.releaseDate < b.releaseDate){
+            if (a.releaseDate < b.releaseDate) {
                 return 1;
             }
             return -1;
         });
         const fragment = document.createDocumentFragment();
         const h2 = document.createElement("h2");
-        h2.textContent = `Las últimas 10 canciones de ${artist.charAt(0).toUpperCase() + artist.slice(1)}:`;
+        h2.textContent = `Last 10 songs of ${artist.charAt(0).toUpperCase() + artist.slice(1)}:`;
         fragment.appendChild(h2);
+        
         for (let i = 0; i < 10; i++) {
             const li = document.createElement("li");
             li.textContent = `${dataObject[i].trackName}`;
             fragment.appendChild(li);
         }
-        document.getElementById("newestSong").appendChild(fragment);
-    } catch (e){
+        
+        $(id).append(fragment);
+        
+    } catch (e) {
         console.log(e);
         console.log("API call error - newestSongs");
     }
 }
-const butt = document.getElementById("butt");
-butt.addEventListener("click",() =>{
-    const newestSearchSongs = () =>{
+
+const newestSongButton = document.getElementById("newestSongButton");
+newestSongButton.addEventListener("click",() => {
+    const newestSearchSongs = () => {
         const search = document.querySelector("#artist")
         artistwait = search.value;
+        id = "#searchNewestSong";
         newestSongs();
     }
-    newestSearchSongs();  
+    newestSearchSongs();
+    $('#searchNewestSong').empty();
+    $('#artist').val('');
 })
 
-const newestgalantisSongs = () =>{
-        artistwait = "galantis";
-        newestSongs();
-        }
+const newestgalantisSongs = () => {
+    artistwait = "galantis";
+    newestSongs();
+}
 newestgalantisSongs();
 
-const newestGagaSongs = () =>{
+const newestGagaSongs = () => {
     artistwait = "lady gaga";
     newestSongs();
 }
 newestGagaSongs();
 
-const searchingPeace = async() =>{
-    try{
+const searchingPeace = async () => {
+    try {
         const response = await fetch("https://itunes.apple.com/search?term=peace&limit=30");  
         //La API retornaba 200 resultados. he puesto límite a lo solicitado, 30.      
         const result = await response.json();
         let dataObject = result.results;
         const fragment = document.createDocumentFragment();
         const h2 = document.createElement("h2");
-        h2.textContent = `30 resultadoss de la búsqueda Peace:`;
+        h2.textContent = `30 results of "Peace":`;
         fragment.appendChild(h2);
         for (let i = 0; i < dataObject.length; i++) {
             const li = document.createElement("li");
-            li.textContent = `La consulta ${[i+1]} es del artista ${dataObject[i].artistName} y corresponde a un ${dataObject[i].wrapperType}`;
+            li.textContent = `Response ${[i+1]} from the artist ${dataObject[i].artistName} media type - ${dataObject[i].wrapperType}`;
             fragment.appendChild(li);
         }
         document.getElementById("searchingPeace").appendChild(fragment);
-    } catch (e){
+    } catch (e) {
         console.log(e);
         console.log("API call error - searchingPeace");
     }
 }
 searchingPeace();
 
-const releaseAdele2021 = async() =>{
-    try{
+const releaseAdele2021 = async () => {
+    try {
         const response = await fetch("https://itunes.apple.com/search?term=adele&media=music&limit=200");        
         const result = await response.json();
         let dataObject = result.results;
         const fragment = document.createDocumentFragment();
         const h2 = document.createElement("h2");
-        h2.textContent = `Los temas de Adele en 2021 son :`;
+        h2.textContent = `Adele's themes in 2021 are:`;
         fragment.appendChild(h2);
         for (let i = 0; i < dataObject.length; i++) {
             const element = dataObject[i];
@@ -96,7 +107,7 @@ const releaseAdele2021 = async() =>{
             }
             document.getElementById("releaseAdele2021").appendChild(fragment);
         }
-    } catch (e){
+    } catch (e) {
         console.log(e);
         console.log("API call error - releaseAdele2021");
     }
@@ -106,37 +117,38 @@ releaseAdele2021();
 // Por falta de conocimiento de la API no he podido encontrar la propiedad ratingIndex, éste es un simulacro del result:
 
 var typewait = "";
-const bestRatingIndex = async() =>{
-    try{
+const bestRatingIndex = async () => {
+    try {
         let type = await typewait;
-        var apiRest = `https://itunes.apple.com/search?term=${type}&ratingIndex&limit=10`;
+        var apiRest = `https://itunes.apple.com/search?term=${type}&ratingIndex&limit=11`;
         const response = await fetch(apiRest);
         const result = await response.json();
         let dataObject = result.results;
         const fragment = document.createDocumentFragment();
         const h2 = document.createElement("h2");
-        h2.textContent = `Los 10 mejor valorados en Itunes:`;
+        h2.textContent = `Top 10 rated ${type} on itunes`;
         fragment.appendChild(h2);
         for (let i = 0; i < 10; i++) {
             const li = document.createElement("li");
-            li.textContent = `${type.charAt(0).toUpperCase() + type.slice(1)} - ${dataObject[i].trackName}`;
+            li.textContent = `${dataObject[i].trackName}`;
             fragment.appendChild(li);
         }
         document.getElementById("bestRatingIndex").appendChild(fragment);
-    } catch (e){
+    } catch (e) {
         console.log(e);
         console.log("API call error - bestRatingIndex");
     }
 }
 
-const bestRatingIndexMovies = () =>{
-    typewait = "movie";
+const bestRatingIndexMovies = () => {
+    typewait = "movies";
     bestRatingIndex();
 }
 bestRatingIndexMovies();
 
-const bestRatingIndexPodcasts = () =>{
+const bestRatingIndexPodcasts = () => {
     typewait = "podcast";
     bestRatingIndex();
-        }
+}
 bestRatingIndexPodcasts();
+});
